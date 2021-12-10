88a7815c03af22094c4747c01aefcb85d62b42dc
rbabarov <90253870+rbabarov@users.noreply.github.com>
2021-12-10 10:30:05 +0500

    Lector and Rewiwer classes added

--- task1.py
+++ task1.py
@@ -11,8 +11,17 @@ class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
-        self.courses_attached = []
-        
+        self.courses_attached = []        
+  
+
+class Lecturer(Mentor):
+    pass 
+ 
+class Reviewer(Mentor):
+    def __init__(self,name,surname):
+        super().__init__(name,surname)
+
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -22,21 +31,16 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-    pass 
- 
-class Reviewer(Mentor):
-    pass
- 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student = Student('Ruoy', 'Eman', 'male')
 best_student.courses_in_progress += ['Python']
  
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+
+cool_reviewer = Reviewer('Bill', 'Gates') 
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
 
