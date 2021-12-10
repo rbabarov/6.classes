3c14e501fcf84d3d6fbe336307c491a4f6fbd7d5
rbabarov <90253870+rbabarov@users.noreply.github.com>
2021-12-10 09:57:19 +0500

    readme added

--- task1.py
+++ task1.py
@@ -21,6 +21,12 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+class Lecturer(Mentor):
+    pass 
+ 
+class Reviewer(Mentor):
+    pass
  
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
@@ -33,3 +39,4 @@ cool_mentor.rate_hw(best_student, 'Python', 10)
 cool_mentor.rate_hw(best_student, 'Python', 10)
  
 print(best_student.grades)
+
